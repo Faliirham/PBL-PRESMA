76a58aa59f61c0657f73cc1341026df3bd9ccaed
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2024-12-01 21:12:09 +0700

    Tambah index Admin

---
+++ Admin/index_Admin.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIPPMA</title>
+    <link rel="stylesheet" href="../style/styleIndexAdmin.css">
+</head>
+<body>
+    <div class="container">
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <div class="profile">
+                <img src="../img/profil.png" alt="Profile" class="profile-img">
+                <h3 class="profile-name">Hallo Admin</h3>
+                <p class="profile-name">2341010101010</p>
+                <p class="profile-name">TEKNIK INFORMASI</p>
+            </div>
+            <nav class="menu">
+                <a href="#" class="menu-item">Beranda</a>
+                <a href="#" class="menu-item">Profile</a>
+                <a href="#" class="menu-item">Input Agenda</a>
+                <a href="#" class="menu-item">Leader Board</a>
+                <a href="#" class="menu-item">Validasi Input</a>
+                <a href="#" class="menu-item">Validasi Message</a>
+            </nav>
+            <footer>
+                <p>© polinema.sch.id</p>
+            </footer>
+        </div>
+
+        <!-- Main Content -->
+        <div class="main-content">
+            <header class="header">
+                <div class="logo">
+                    <img src="../img/logo_SIPPMA.png" alt="Logo">
+                </div>
+                <h1 class="teks">
+                    IPP<span class="highlight">MA</span>
+                </h1>
+                <div class="logout">
+                    <button>Logout</button>
+                </div>
+            </header>
+            <section class="welcome">
+                <h2>SELAMAT DATANG "Admin"</h2>
+            </section>
+            <div class="content">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
