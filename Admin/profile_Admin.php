f6818bd0d780678941f903564b69bca269d368de
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2024-12-04 16:48:44 +0700

    Profile done

--- Admin/profile_Admin.php
+++ Admin/profile_Admin.php
@@ -58,18 +58,20 @@
                 <h1>Data diri Admin</h1>
                 <div class="profile-content">
                     <div class="profile-picture">
-                        <img src="https://via.placeholder.com/100" alt="Profile Picture">
+                        <img src="https://via.placeholder.com/130" alt="Profile Picture">
+                        <div>
                         <button class="profile-btn-ubah">Ubah Foto</button>
+                        </div>
                     </div>
                     <div class="profile-form">
                         <form action="#" method="post">
                             <div class="profile-form-group">
                                 <label for="nama">Nama</label>
-                                <input type="text" id="nama" value="Halo Admin">
+                                <input type="text" id="nama" value="Hallo Admin">
                             </div>
                             <div class="profile-form-group">
                                 <label for="nip">NIP</label>
-                                <input type="text" id="nip" value="220247063888">
+                                <input type="text" id="nip" value="22047463888">
                             </div>
                             <div class="profile-form-group">
                                 <label for="studi">Studi</label>
