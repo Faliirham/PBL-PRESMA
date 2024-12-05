7c7c8fcc1763942d536099fd33218d55ec9abd45
anandarahmawati24 <rahmawatiananda571@gmail.com>
2024-12-05 10:08:26 +0700

    tambah dropdown

--- Admin/validasiMessage_Admin.php
+++ Admin/validasiMessage_Admin.php
@@ -72,40 +72,56 @@
                             <td>Lomba PKM KC PIMNAS Tahun 2023</td>
                             <td>Juara 1</td>
                             <td>Nasional</td>
-                            <td class="validasi-message-btn">
-                                <button class="validasi-message-btn-validasi">Validasi</button>
-                                <button class="validasi-message-btn-icon">👁</button>
-                            </td>
+                            <td class="validasi-message-dropdown">
+                            <select class="validasi-dropdown" name="validasiStatus">
+                                <option value="validasi">Validasi</option>
+                                <option value="ditolak">Ditolak</option>
+                                <option value="diterima">Diterima</option>
+                            </select>
+                            <button class="validasi-message-btn-icon">👁</button>
+                        </td>
                         </tr>
                         <tr>
                             <td>2</td>
                             <td>Lomba PKM KC PIMNAS Tahun 2023</td>
                             <td>Juara 2</td>
                             <td>Nasional</td>
-                            <td class="validasi-message-btn">
-                                <button class="validasi-message-btn-ditolak">Ditolak</button>
-                                <button class="validasi-message-btn-icon">👁</button>
-                            </td>
+                            <td class="validasi-message-dropdown">
+                            <select class="validasi-dropdown" name="validasiStatus">
+                                <option value="validasi">Validasi</option>
+                                <option value="ditolak">Ditolak</option>
+                                <option value="diterima">Diterima</option>
+                            </select>
+                            <button class="validasi-message-btn-icon">👁</button>
+                        </td>
                         </tr>
                         <tr>
                             <td>3</td>
                             <td>Lomba PKM KC PIMNAS Tahun 2023</td>
                             <td>Juara 3</td>
                             <td>Nasional</td>
-                            <td class="validasi-message-btn">
-                                <button class="validasi-message-btn-diterima">Diterima</button>
-                                <button class="validasi-message-btn-icon">👁</button>
-                            </td>
+                            <td class="validasi-message-dropdown">
+                            <select class="validasi-dropdown" name="validasiStatus" onchange="this.setAttribute('value', this.value);">
+                                <option value="validasi">Validasi</option>
+                                <option value="ditolak">Ditolak</option>
+                                <option value="diterima">Diterima</option>
+                            </select>
+                            <button class="validasi-message-btn-icon">👁</button>
+                        </td>
                         </tr>
                         <tr>
                             <td>4</td>
                             <td>Lomba PKM KC PIMNAS Tahun 2023</td>
                             <td>Harapan 1</td>
                             <td>Nasional</td>
-                            <td>
-                                <button class="validasi-message-btn-diterima">Diterima</button>
-                                <button class="validasi-message-btn-icon">👁</button>
-                            </td>
+                            <td class="validasi-message-dropdown">
+                            <select class="validasi-dropdown" name="validasiStatus">
+                                <option value="validasi">Validasi</option>
+                                <option value="ditolak">Ditolak</option>
+                                <option value="diterima">Diterima</option>
+                            </select>
+                            <button class="validasi-message-btn-icon">👁</button>
+                        </td>
                         </tr>
                     </tbody>
                 </table>
