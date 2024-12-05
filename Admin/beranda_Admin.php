93a8fe2dc684b7039d3a4f560de06f7feeb2d20c
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2024-12-05 09:29:35 +0700

    icon

--- Admin/beranda_Admin.php
+++ Admin/beranda_Admin.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIPPMA</title>
     <link rel="stylesheet" href="../style/styleAdmin.css">
+    <title>SIPPMA</title>
+    <link rel="icon" type="image/png" href="img/logo_SIPPMA.png">
 </head>
 <body>
     <!-- Sidebar -->
