cb0d61b06c36007a9a470b1267a985167eb27219
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2024-12-05 09:52:47 +0700

    ,

--- landing_page.php
+++ landing_page.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styleLandingPage.css">
+  <link rel="stylesheet" href="style/styleLandingPage.css">
   <title>SIPPMA</title>
   <link rel="icon" type="image/png" href="img/logo_SIPPMA.png">
 </head>
